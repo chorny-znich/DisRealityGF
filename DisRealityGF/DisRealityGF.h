b52379c2bd6ec9c2a8df8ba8d97981ac5d74a89c
chorny-znich <vargar.dt@gmail.com>
2023-03-06 22:32:21 +0300

    - add common header file

---
+++ DisRealityGF/DisRealityGF.h
@@ -0,0 +1,16 @@
+//=================================================================================================
+//	DisReality game framework																	                                  ===
+//	Framework based on SFML library for creating 2D games										                    ===
+//	Common header file for connection of all DisRealityGF's header files 	                      ===
+//=================================================================================================
+
+#pragma once
+/*
+#include "button.h"
+#include "dynamic_object.h"
+#include "engine.h"/*
+#include "engine_data.h"
+#include "screen.h"
+#include "static_object.h"
+#include "text_manager.h"
+#include "time_bar.h"*/
