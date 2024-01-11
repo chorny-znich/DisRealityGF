3ad000bf5bbddc8ae211e11e1702c7dcf97e6a67
chorny-znich <vargar.dt@gmail.com>
2024-01-11 15:52:32 +0300

    feat: add headers to the common header framework file

--- DisRealityGF/DisRealityGF.h
+++ DisRealityGF/DisRealityGF.h
@@ -1,7 +1,7 @@
 #pragma once
 
-/*#include "engine.h"
-#include "screen.h"
+#include "engine.h"
+#include "screen.h" /*
 #include "text_manager.h"
 #include "button.h" */
 #include "engine_utility.h"
